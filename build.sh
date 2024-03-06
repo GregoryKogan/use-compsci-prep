419dda3fd2c905e1e97b6d446cc86c1291115606
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-03-06 22:08:25 +0300

    Update references and build script. Improve lesson 1 text

--- build.sh
+++ build.sh
@@ -21,24 +21,24 @@ mkdir out
 # Build output
 args=(
   --pdf-engine=xelatex
-#   -f markdown+implicit_figures
+  # -f markdown+implicit_figures
   --lua-filter=filters/strikeout.lua
   --lua-filter=filters/underline.lua
   -V block-headings
   --template=templates/eisvogel.tex
   --listings
   --toc
-  --toc-depth=4
+  --toc-depth=3
   --highlight-style=pygments
-#   --number-sections
-#   --include-in-header=templates/header.tex
-#   --include-before-body=templates/before-body.tex
-#   --include-after-body=templates/after-body.tex
+  # --number-sections
+  # --include-in-header=templates/header.tex
+  # --include-before-body=templates/before-body.tex
+  # --include-after-body=templates/after-body.tex
   --metadata-file=metadata.yaml
   --citeproc
-#   --bibliography=references.bib
-#   --csl=templates/ieee.csl
-#   --verbose
+  --bibliography=references.bib
+  # --csl=templates/ieee.csl
+  # --verbose
 )
 
 pandoc src/*.md -o out/theory.pdf ${args[@]}
